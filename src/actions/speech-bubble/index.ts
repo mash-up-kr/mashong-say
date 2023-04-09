71366b544000861e2b9d2711227aeea710a8b59c
seohyun0120 <sarahseohyun@gmail.com>
2023-04-09 16:42:30 +0900

    Feat: 여러 줄 지원, 텍스트 중앙 정렬

--- src/actions/speech-bubble/index.ts
+++ src/actions/speech-bubble/index.ts
@@ -15,10 +15,29 @@ export class SpeechBubble {
     const bubble = bubbleTypeDic[bubbleType];
     return format(input, bubble);
   }
-    }
+}
 
-    return input;
+function format(text: string, btype: string[]): string {
+  const lines = text.split(/\r?\n/);
+
+  const maxLength = Math.max(...lines.map(line => getStringLength(line)));
+
+  const center = lines.map(line => {
+    const spaces = maxLength - getStringLength(line);
+    const left = Math.floor(spaces / 2);
+    const right = spaces - left;
+    return ' '.repeat(Math.max(0, left)) + line + ' '.repeat(Math.max(0, right));
+  });
+
+  const updown = ' ' + '-'.repeat(maxLength + 2) + ' ';
+  const bubble = [updown];
+
+  for (let i = 0; i < center.length; i++) {
+    bubble.push(`${btype[0]} ${center[i]} ${btype[1]}`);
   }
+  bubble.push(updown);
+
+  return bubble.join('\n');
 }
 
 function getStringLength(text: string): number {
