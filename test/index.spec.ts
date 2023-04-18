01dffe6d200649a984d0062021884567f85a0fe8
seohyun0120 <sarahseohyun@gmail.com>
2023-04-18 20:25:55 +0900

    Feat: 노드팀 매숑 테스트 추가

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,7 @@
 import { mashong } from "../src";
 
 describe("매숑 테스트", () => {
-  it("브랜딩 메숑 성공 테스트", () => {
+  it("브랜딩 매숑 성공 테스트", () => {
     const output = mashong("hi", { platform: "branding" });
     expect(output).toMatchInlineSnapshot(`
 " ---- 
@@ -28,6 +28,32 @@ describe("매숑 테스트", () => {
                                                            
                                                            
                                                                                         "
+`);
+  });
+
+  it("노드팀 매숑 성공 테스트", () => {
+    const output = mashong("hi", { platform: "node" });
+    expect(output).toMatchInlineSnapshot(`
+" ---- 
+| hi |
+ ---- 
+        \\
+         \\
+           _____                           _____
+          /     \\__                       /      \\
+         /         \\___              ____/        \\
+        |               \\           /             |
+         \\               |_________|            _/
+          \\_____                            ___/
+                |                          |
+               /                            \\ 
+              /      ████████████████████    \\
+             /       -█▄▀▄▀███──██▄▀▄▀██-     \\
+            |         -▀▄█▄█▀────▀▄█▄█▀        |        
+            |                                  |      
+             \\________________________________/
+
+"
 `);
   });
 });
